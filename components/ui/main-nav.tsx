5a07c9647ec42ef64ddcfb3e63469cd506d825d1
shafu <selfouly@gmail.com>
2024-05-30 19:19:59 +0200

    eth price

--- components/ui/main-nav.tsx
+++ components/ui/main-nav.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 
-import { cn } from "@/lib/utils";
+import {cn} from "@/lib/utils";
+import useEthPrice from "@/hooks/useEthPrice";
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const {ethPrice} = useEthPrice();
+
   return (
     <nav
       className={cn(
@@ -20,6 +23,14 @@ export function MainNav({
       >
         DYAD
       </Link>
+      <div className="flex text-gray-200">
+        <div>ETH: $</div>
+        <div>{ethPrice}</div>
+      </div>
+      {/* <div className="flex "> */}
+      {/*   <div>KEROSENE DV: $</div> */}
+      {/*   <div>{ethPrice}</div> */}
+      {/* </div> */}
       {/* <Link */}
       {/*   href="/vaults" */}
       {/*   className="text-sm font-bold text-muted-foreground transition-colors hover:text-primary" */}
