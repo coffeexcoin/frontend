7e1174da69ad98f189ac60f3cdbdb465dbdd73a3
shafu <selfouly@gmail.com>
2024-06-06 18:46:40 +0200

    rest

--- components/earn-kerosene.tsx
+++ components/earn-kerosene.tsx
@@ -17,6 +17,7 @@ import KeroseneCardMerkle from "@/components/KeroseneCard/KeroseneCardMerkle";
 import StakingAbi from "@/abis/Staking.json";
 import useKerosenePrice from "@/hooks/useKerosenePrice";
 import NoteCardsContainer from "../components/reusable/NoteCardsContainer";
+import {ClaimModalContent} from "./claim-modal-content";
 
 export function EarnKeroseneContent() {
   const {address, isConnected} = useAccount();
@@ -56,7 +57,7 @@ export function EarnKeroseneContent() {
 
   if (isConnected) {
     return (
-      <>
+      <div className="flex flex-col space-y-4">
         <NoteCardsContainer>
           <div className="text-sm font-semibold text-[#A1A1AA]">
             <div className="flex w-full flex justify-between items-center">
@@ -65,6 +66,63 @@ export function EarnKeroseneContent() {
                 Step 1
               </div>
               <div >Claim a Note or buy on OpenSea</div>
+            </div>
+            <div className="flex justify-between mt-[32px] w-full">
+              <div className="w-full">
+                <ClaimModalContent />
+              </div>
+            </div>
+          </div>
+        </NoteCardsContainer>
+        <NoteCardsContainer>
+          <div className="text-sm font-semibold text-[#A1A1AA]">
+            <div className="flex w-full flex justify-between items-center">
+
+              <div className="text-2xl text-[#FAFAFA]  ">
+                Step 2
+              </div>
+              <div >Deposit wETH and/or wstETH and mint DYAD</div>
+            </div>
+            <div className="flex justify-between mt-[32px] w-full">
+              <div className="w-full">
+                <ButtonComponent>
+                  Switch to Manage Notes tab
+                </ButtonComponent>
+              </div>
+            </div>
+          </div>
+        </NoteCardsContainer>
+        <NoteCardsContainer>
+          <div className="text-sm font-semibold text-[#A1A1AA]">
+            <div className="flex w-full flex justify-between items-center">
+
+              <div className="text-2xl text-[#FAFAFA]  ">
+                Step 3
+              </div>
+              <div >Provide liquidity to USDC - DYAD on Uniswap v3</div>
+            </div>
+            <div className="flex justify-between mt-[32px] w-full">
+              <div className="w-full">
+                <ButtonComponent
+                  onClick={() =>
+                    window.open(
+                      "https://app.uniswap.org/add/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/0xFd03723a9A3AbE0562451496a9a394D2C4bad4ab/500?minPrice=1.003256&maxPrice=1.005265"
+                    )
+                  }
+                >
+                  LP USDC - DYAD on Uniswap V3
+                </ButtonComponent>
+              </div>
+            </div>
+          </div>
+        </NoteCardsContainer>
+        <NoteCardsContainer>
+          <div className="text-sm font-semibold text-[#A1A1AA]">
+            <div className="flex w-full flex justify-between items-center">
+
+              <div className="text-2xl text-[#FAFAFA]  ">
+                That's it. No staking necessary.
+              </div>
             </div>
             <div className="flex justify-between mt-[32px] w-full">
               <div className="w-full">
@@ -81,7 +139,7 @@ export function EarnKeroseneContent() {
             </div>
           </div>
         </NoteCardsContainer>
-      </>
+      </div>
     );
   }
 
