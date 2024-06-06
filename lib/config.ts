96ac7613a0b8b4c069fc4edc3a908402354b73a1
shafu <selfouly@gmail.com>
2024-06-06 14:56:48 +0200

    switch to mainnet

--- lib/config.ts
+++ lib/config.ts
@@ -3,7 +3,7 @@ import {mainnet, sepolia, anvil} from "viem/chains";
 import {defaultWagmiConfig} from "@web3modal/wagmi";
 
 export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_ID;
-export const defaultChain = anvil;
+export const defaultChain = mainnet;
 
 if (!projectId) throw new Error("Project ID is not defined");
 
