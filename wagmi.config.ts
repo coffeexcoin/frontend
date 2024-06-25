c42874a3892931088432e1f0dcbcaa5640bfcbf7
Coffee☕️ <coffee@coffeebot.dev>
2024-06-25 16:40:40 -0400

    remove unused import

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -8,7 +8,7 @@ import { vaultManagerAbi } from "@/lib/abi/VaultManager";
 import { vaultAbi } from "@/lib/abi/Vault";
 import { paymentsAbi } from "@/lib/abi/Payments";
 import { merkleClaimAbi } from "@/lib/abi/MerkleClaim";
-import { erc20Abi, zeroAddress } from "viem";
+import { erc20Abi } from "viem";
 import { angleDistributorAbi } from "./lib/abi/AngleDistributor";
 import { keroseneDnftClaimAbi } from "./lib/abi/KeroseneDnftClaim";
 
